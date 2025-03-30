6fbbf33d959811a39c580371889602df4867414a
lord ahmed <lordahmed1612@gmail.com>
2025-03-30 06:01:05 +0200

    90 accuracy model

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow import keras
+import random
+import os
+import cv2 as cv
 
-# Create a Sequential model
-model = Sequential()
+husky_train=os.listdir('data/train/husky')
+wolf_train=os.listdir('data/train/wolf')
 
-# Add layers to the model
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(10, activation='softmax'))  # Assuming 10 classes for classification
-
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Summary of the model
-model.summary()
+print(random.sample(husky_train,4))
